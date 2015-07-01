349c0cfbf08e08b3360fcab09133b26651a86177
Sämir Hamadi <samir.hamadi@elisa.fi>
2015-07-01 11:25:37 +0300

    It is a better practice to include the throwable to the exception

--- src/main/java/fi/samssi/fibonacci/calculator/OverFlowUtil.java
+++ src/main/java/fi/samssi/fibonacci/calculator/OverFlowUtil.java
@@ -8,7 +8,7 @@ public class OverFlowUtil {
         try {
             result = Math.addExact(x, y);
         } catch (ArithmeticException ae) {
-            throw new FibonacciCalculatorException("This calculator can only calculate values up to: " + Long.MAX_VALUE + " BigInteger implementation would enable bigger number with the possibility to run out of memory.");
+            throw new FibonacciCalculatorException("This calculator can only calculate values up to: " + Long.MAX_VALUE + " BigInteger implementation would enable bigger number with the possibility to run out of memory.", ae);
         }
         return result;
     }
--- src/main/java/fi/samssi/fibonacci/exception/FibonacciCalculatorException.java
+++ src/main/java/fi/samssi/fibonacci/exception/FibonacciCalculatorException.java
@@ -2,7 +2,7 @@ package fi.samssi.fibonacci.exception;
 
 public class FibonacciCalculatorException extends RuntimeException {
 
-    public FibonacciCalculatorException(String message) {
-        super(message);
+    public FibonacciCalculatorException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
